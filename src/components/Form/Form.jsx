f0e7092e8c697c1e976b559e94af2d0c8e391e87
hpteach2021 <hooper.rob@gmail.com>
2021-11-11 21:23:10 +0000

    tweaked media queries to include iPad

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,18 +15,22 @@ const Form = (props) => {
     <>
     <form action="/action_page.php">
       <div class="container">
+
         <div className="container__item container__item--name">
           <label className="container__label" for="name">
             Name
           </label>
           <input className="container__box" type="text" name="name" required />
         </div>
+
+
         <div className="container__item container__item--email">
           <label className="container__label" for="email">
             Email
           </label>
           <input className="container__box" type="text" name="email" required />
         </div>
+
         <div className="container__item container__item--password">
           <label className="container__label" for="password">
             Password
@@ -39,24 +43,30 @@ const Form = (props) => {
           />
           <button className="container__show-button"onClick={togglePassword}>show</button>
         </div>
+
+
         <div className="container__item  container__item--phone">
           <label className="container__label" for="phone">
             Phone number
           </label>
-          <input className="container__box--phone" type="tel" name="phone" />
+          <input className="container__box--phone container__box" type="tel" name="phone" />
         </div>
+
         <div className="container__item container__item--info">
           <label className="container__label" for="moreInfo">
             How did you hear about us?
           </label>
-          <textarea className="container__item container__box--info" type="text" cols="40" 
+          <textarea className="container__box container__box--info" type="text" cols="40" 
        rows="5"name="moreInfo" />
         </div>
+
+
         <div className="container__item container__item--confirm" onClick={toggleSignIn} > 
           <img className="container__button"src={confirm} alt="" />
           <div className="container__button-text">Confirm</div>
         </div>
       </div>
+
       
     </form>
     </>
