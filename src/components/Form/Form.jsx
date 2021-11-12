83984ccf1bdc14afd590438e12e558a5d7e4706d
hpteach2021 <hooper.rob@gmail.com>
2021-11-12 13:53:09 +0000

    naming conventions improved

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,56 +14,56 @@ const Form = (props) => {
   return (
     <>
     <form action="/action_page.php">
-      <div class="container">
+      <div class="form">
 
-        <div className="container__item container__item--name">
-          <label className="container__label" for="name">
+        <div className="form__item form__item--name">
+          <label className="form__label" for="name">
             Name
           </label>
-          <input className="container__box" type="text" name="name" required />
+          <input className="form__box" type="text" name="name" required />
         </div>
 
 
-        <div className="container__item container__item--email">
-          <label className="container__label" for="email">
+        <div className="form__item form__item--email">
+          <label className="form__label" for="email">
             Email
           </label>
-          <input className="container__box" type="text" name="email" required />
+          <input className="form__box" type="text" name="email" required />
         </div>
 
-        <div className="container__item container__item--password">
-          <label className="container__label" for="password">
+        <div className="form__item form__item--password">
+          <label className="form__label" for="password">
             Password
           </label>
           <input
-            className="container__box"
+            className="form__box"
             type={passwordShown ? "text" : "password"}
             name="password"
             required
           />
-          <button className="container__show-button"onClick={togglePassword}>show</button>
+          <button className="form__show-button"onClick={togglePassword}>show</button>
         </div>
 
 
-        <div className="container__item  container__item--phone">
-          <label className="container__label" for="phone">
+        <div className="form__item  form__item--phone">
+          <label className="form__label" for="phone">
             Phone number
           </label>
-          <input className="container__box--phone container__box" type="tel" name="phone" />
+          <input className="form__box--phone form__box" type="tel" name="phone" />
         </div>
 
-        <div className="container__item container__item--info">
-          <label className="container__label" for="moreInfo">
+        <div className="form__item form__item--info">
+          <label className="form__label" for="moreInfo">
             How did you hear about us?
           </label>
-          <textarea className="container__box container__box--info" type="text" cols="40" 
+          <textarea className="form__box form__box--info" type="text" cols="40" 
        rows="5"name="moreInfo" />
         </div>
 
 
-        <div className="container__item container__item--confirm" onClick={toggleSignIn} > 
-          <img className="container__button"src={confirm} alt="" />
-          <div className="container__button-text">Confirm</div>
+        <div className="form__item form__item--confirm" onClick={toggleSignIn} > 
+          <img className="form__button"src={confirm} alt="" />
+          <div className="form__button-text">Confirm</div>
         </div>
       </div>
 
